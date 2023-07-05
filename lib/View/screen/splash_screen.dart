219d53a0742d15fd92959a650c4f56be243d4452
fahis <afup7808@gmail.com>
2023-07-05 19:57:00 +0530

    splash screen

--- lib/View/screen/splash_screen.dart
+++ lib/View/screen/splash_screen.dart
@@ -1,10 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:weather/View/screen/dashboard_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Timer(
+        const Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => DashboardScreen())));
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Text(
+            'Artifitia Flutter Assignment',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
